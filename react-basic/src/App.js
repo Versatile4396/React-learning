ff16031fbf0cb59564b0631929177d2a5704c3e4
huangmande <huangmande@bytedance.com>
2024-02-21 09:50:53 +0800

    feat:learning

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="App">this si app</div>;
-}
+import { useState } from "react";
 
+const btnClick2 = (event) => {
+  console.log("btnClick2");
+};
+const list = [
+  { id: 1, name: "zhangsan" },
+  { id: 2, name: "zhangsan" },
+  { id: 3, name: "zhangsan" },
+  { id: 4, name: "zhangsan" },
+];
+function App() {
+  const [count, setCount] = useState(0);
+  const btnClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div onClick={btnClick} className="App">
+      {count}
+      <ul>
+        {list.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
 export default App;
